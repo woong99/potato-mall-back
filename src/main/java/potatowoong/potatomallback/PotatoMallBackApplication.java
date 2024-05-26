86d659e0d1ec2ee0e5f72f1d1b5ce2a34ec1e781
woong99 <wg.jeong@springworks.co.kr>
2024-05-26 11:16:33 +0900

    project init

---
+++ src/main/java/potatowoong/potatomallback/PotatoMallBackApplication.java
@@ -0,0 +1,13 @@
+package potatowoong.potatomallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoMallBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoMallBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/potatowoong/potatomallback/PotatoMallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package potatowoong.potatomallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoMallBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
