419999fe18fd9277fea1e5ddaa65150074241490
JeongWoongGyo <wg.jeong@springworks.co.kr>
2024-06-10 21:27:29 +0900

    fix: front 서버 url 변경에 따른 cors 설정 변경

--- src/main/java/potatowoong/potatomallback/config/security/WebConfig.java
+++ src/main/java/potatowoong/potatomallback/config/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://potato-mall-front.s3-website.ap-northeast-2.amazonaws.com")
+            .allowedOrigins("http://localhost:3000", "https://www.potato-woong.site")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .exposedHeaders("*")
