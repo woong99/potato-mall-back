54c52510c109f2d1333cdfe11c606fac6846505c
JeongWoongGyo <wg.jeong@springworks.co.kr>
2024-06-11 20:48:11 +0900

    feat: SSL 적용을 위해 443포트 추가로 개방

--- src/main/java/potatowoong/potatomallback/config/AdditionalTomcatConfiguration.java
+++ src/main/java/potatowoong/potatomallback/config/AdditionalTomcatConfiguration.java
@@ -1,7 +1,6 @@
 package potatowoong.potatomallback.config;
 
 import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -10,15 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AdditionalTomcatConfiguration {
 
-    @Value("${server.docs-port}")
-    private String docsPort;
-
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> additionalConnector() {
         return factory -> {
-            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-            connector.setPort(Integer.parseInt(docsPort));
-            factory.addAdditionalTomcatConnectors(connector);
+            Connector sslConnector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            sslConnector.setPort(443);
+
+            factory.addAdditionalTomcatConnectors(sslConnector);
         };
     }
 
