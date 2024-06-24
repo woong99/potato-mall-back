08015ecc630e2441544a1f33c4c6c5d4fda5680c
JeongWoongGyo <wg.jeong@springworks.co.kr>
2024-06-24 10:38:09 +0900

    fix: AccessToken 재발급 시 권한을 잘못 부여하고 있던 오류 수정

--- src/main/java/potatowoong/potatomallback/domain/auth/service/TokenRefreshService.java
+++ src/main/java/potatowoong/potatomallback/domain/auth/service/TokenRefreshService.java
@@ -26,30 +26,30 @@ public class TokenRefreshService {
 
     public AccessTokenDto adminRefresh(HttpServletRequest request) {
         final String refreshToken = CookieUtils.getCookieValue(request.getCookies(), TokenName.ADMIN_REFRESH_TOKEN.name());
-        return refresh(refreshToken);
+        return refresh(refreshToken, Role.ROLE_ADMIN);
     }
 
     public AccessTokenDto userRefresh(HttpServletRequest request) {
         final String refreshToken = CookieUtils.getCookieValue(request.getCookies(), TokenName.USER_REFRESH_TOKEN.name());
-        return refresh(refreshToken);
+        return refresh(refreshToken, Role.ROLE_USER);
     }
 
     /**
      * Refresh Token으로 Access Token 갱신
      */
-    private AccessTokenDto refresh(final String refreshToken) {
+    private AccessTokenDto refresh(final String refreshToken, Role role) {
         if (StringUtils.isBlank(refreshToken)) {
             throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
 
-        // Redis에서 Refresh Token으로 Admin ID 가져오기
+        // Redis에서 Refresh Token으로 ID 가져오기
         String userName = redisTemplate.opsForValue().get(refreshToken);
         if (StringUtils.isBlank(userName)) {
             throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
 
         // Authentication 객체 생성
-        Authentication authentication = new UsernamePasswordAuthenticationToken(userName, "", List.of(Role.ROLE_ADMIN::name));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userName, "", List.of(role::name));
 
         // 인증 정보를 기반으로 JWT Token 생성
         return jwtTokenProvider.generateAccessToken(authentication);
